f5f5a1830d3debea365241fd579bee1841235092
Dmytro Melnyk <dmytro.melnyk@vaimo.com>
2026-03-30 15:20:52 +0300

    Set gate to pre commit. Update parameters

--- jest.config.js
+++ jest.config.js
@@ -14,10 +14,10 @@ module.exports = {
   collectCoverageFrom: ['src/**/*.{ts,tsx}', '!src/**/*.d.ts'],
   coverageThreshold: {
     global: {
-      branches: 80,
-      functions: 80,
-      lines: 80,
-      statements: 80,
+      branches: 15,
+      functions: 35,
+      lines: 25,
+      statements: 25,
     },
   },
 };
